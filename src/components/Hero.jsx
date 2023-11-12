8a5b7a7d2d7583c16b8a8a23bf67476d7bff4538
Elisha <elishakibet67@gmail.com>
2023-11-12 13:02:32 +0300

    Added classnames to various elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,15 @@ import photogrid from "../images/photo-grid.png"
 
 function Hero (){
     return (
-        <>
-            <div>
+        <div className="hero--section">
+            <div className="hero--photo">
                 <img src={photogrid} alt="section photogrid" />
             </div>
-            <div>
-
+            <div className="hero-contents">
+                <h3 className="contents--title">Online Experiences</h3>
+                <p className="contents--paragraph">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</p>
             </div>
-        </>
+        </div>
     )
 }
 
