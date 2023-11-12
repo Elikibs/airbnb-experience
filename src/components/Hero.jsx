1ee15b0134ce9867690ce1149482cb9c0a034c66
Elisha <elishakibet67@gmail.com>
2023-11-12 12:55:12 +0300

    Created Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import photogrid from "../images/photo-grid.png"
+
+function Hero (){
+    return (
+        <>
+            <div>
+                <img src={photogrid} alt="section photogrid" />
+            </div>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+export default Hero;
