6130472f27ef540ab0116e1d75408746a8157531
Elisha <elishakibet67@gmail.com>
2023-11-13 07:31:22 +0300

    Added card props and styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
 import katieimage from "../images/katie-zaferes.png"
+import starimage from "../images/star.png"
 
 function Card (){
     return (
-        <div>
-            <img src={katieimage} alt="" />
-            <p></p>
-            <p></p>
-            <p></p>
+        <div className="card">
+            <img src={katieimage} alt="card-image"  className="card--image"/>
+            <div className="card--stats">
+                <img src={starimage} alt="star-image" className="card--star"/>
+                <span>5.0</span>
+                <span className="gray">(6) . </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><strong>From $136</strong> / person</p>
         </div>
     )
 }
