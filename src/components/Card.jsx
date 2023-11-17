1d6fccb6640f936cffc3c50f17f2d0dc73d81449
Elisha <elishakibet67@gmail.com>
2023-11-17 16:09:04 +0300

    Added conditional rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,15 @@
 import starimage from "/images/star.png"
 
 function Card (props){
+    let badgeText
+    if (props.openSpots === 0){
+        badgeText="SOLD OUT"
+    } else if(props.location === "Online"){
+        badgeText="ONLINE"
+    }
     return (
         <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={`/images/${props.img}`} alt="card-image"  className="card--image"/>
             <div className="card--stats">
                 <img src={starimage} alt="star-image" className="card--star"/>
