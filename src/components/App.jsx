cace530b60d5658444d3c404490f36cf4a33a3b0
Elisha <elishakibet67@gmail.com>
2023-11-17 15:47:21 +0300

    add a prop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,7 @@ function App() {
                 location={data.location}
                 title={data.title}
                 price={data.price}
+                openSpots={data.openSpots}
           />
   })
   return (
