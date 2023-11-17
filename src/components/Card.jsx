81826fc29671bc3ec5d4202a3bd8a0b9a603c457
Elisha <elishakibet67@gmail.com>
2023-11-17 12:25:23 +0300

    update on props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,9 @@
-import katieimage from "../images/katie-zaferes.png"
 import starimage from "../images/star.png"
 
 function Card (props){
     return (
         <div className="card">
-            <img src={katieimage} alt="card-image"  className="card--image"/>
+            <img src={props.img} alt="card-image"  className="card--image"/>
             <div className="card--stats">
                 <img src={starimage} alt="star-image" className="card--star"/>
                 <span>{props.rating}</span>
